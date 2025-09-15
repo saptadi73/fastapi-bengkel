7a51e4a54854032781335da6fc9313cda5314b91
saptadi73 <saptadi.nurfarid@gmail.com>
2025-09-15 15:16:29 +0700

    perubahan models

--- database_baru/inventory_postgres.sql
+++ database_baru/inventory_postgres.sql
@@ -1,13 +1,3 @@
-CREATE TABLE IF NOT EXISTS inventory (
-    id UUID PRIMARY KEY,
-    product_id UUID REFERENCES product(id),
-    quantity NUMERIC(10,2) NOT NULL,
-    price NUMERIC(10,2) NOT NULL,
-    cost NUMERIC(10,2) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP
-);
-
 CREATE TABLE IF NOT EXISTS product_moved_history (
     id UUID PRIMARY KEY,
     product_id UUID REFERENCES product(id),
